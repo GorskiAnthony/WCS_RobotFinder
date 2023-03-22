ba33d66197567101a5cf4d4a88ee4ffdf6ed7bb6
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-03-22 19:46:12 +0100

    step1: création des fontions plus appel de la fonction dans mon fichier main.js

--- js/createCard.js
+++ js/createCard.js
@@ -1,1 +1,34 @@
 // Your code
+function createCard(data) {
+	let html = `
+    <!-- CARD EXAMPLE -->
+    <div class="user-info">
+        <p><strong>Nom d'utilisateur :</strong> ${data.name}</p>
+        <p><strong>Email :</strong> ${data.email}</p>
+        <p>
+            <strong>Adresse :</strong> ${data.address.street}, ${data.address.suite},
+            ${data.address.city}, ${data.address.zipcode}
+        </p>
+        <p><strong>Téléphone :</strong> ${data.phone}</p>
+        <p><strong>Nom de l'entreprise :</strong> ${data.company.name}</p>
+        <p>
+            <strong>Slogan de l'entreprise :</strong> ${data.company.catchPhrase}
+        </p>
+        <p>
+            <strong>Coordonnées géographiques :</strong> ${data.address.geo.lat}, ${data.address.geo.lng}
+        </p>
+    </div>
+    <!-- END CARDS EXEMPLE -->`;
+
+	return html;
+}
+
+function displayHTML(users) {
+	let html = "";
+	users.forEach((user) => {
+		html += createCard(user);
+	});
+	return html;
+}
+
+export default displayHTML;
--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 console.log("Hello World!");
 import users from "./datas.js";
+import displayHTML from "./createCard.js";
 
 const CARDS = document.querySelector(".cards");
 
-console.log(users);
+CARDS.innerHTML = displayHTML(users);
+
 /**
  * Étape 1 :
  *
- * Créer un fichier card.js, ce fichier permettra d'afficher toutes les cartes concernant nos robots
- * Dans ce fichier, vous allez créer 2 fonctions
+ * Dans le fichier createCard, vous allez créer 2 fonctions
  * createCard(data) & displayHtml(users)
  */

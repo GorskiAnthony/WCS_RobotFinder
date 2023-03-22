bdbada64ddc90a158319194de1c29bbd2ee98a3f
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-03-22 19:57:01 +0100

    step2: ajout d'un écouteur d'evenement

--- js/main.js
+++ js/main.js
@@ -3,12 +3,27 @@ import users from "./datas.js";
 import displayHTML from "./createCard.js";
 
 const CARDS = document.querySelector(".cards");
+const SEARCH = document.querySelector("#search");
 
-CARDS.innerHTML = displayHTML(users);
+let userSearch = [];
+
+SEARCH.addEventListener("keyup", () => {
+	userSearch = users.filter((user) =>
+		user.name.toLowerCase().includes(SEARCH.value.toLowerCase())
+	);
+	CARDS.innerHTML = displayHTML(userSearch);
+});
 
 /**
  * Étape 1 :
  *
  * Dans le fichier createCard, vous allez créer 2 fonctions
  * createCard(data) & displayHtml(users)
+ *
+ * Étape 2 :
+ *
+ * Récupérer l'input qui permet de rechercher une personne
+ * On écoute la valeur et on filtre notre tableau d'utilisateur avec la valeur saisie par l'utilisateur
  */
+
+CARDS.innerHTML = displayHTML(users);

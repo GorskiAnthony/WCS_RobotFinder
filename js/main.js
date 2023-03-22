413a74b684f14497704135bd0b7302a67cb3c8e6
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-03-22 19:36:11 +0100

    Ajout des robots

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("Hello World!");
+import users from "./datas.js";
+
+const CARDS = document.querySelector(".cards");
+
+console.log(users);
+/**
+ * Étape 1 :
+ *
+ * Créer un fichier card.js, ce fichier permettra d'afficher toutes les cartes concernant nos robots
+ * Dans ce fichier, vous allez créer 2 fonctions
+ * createCard(data) & displayHtml(users)
+ */
